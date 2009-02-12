eea0b78dcddc35601cb4f7eda801b1c99073dedf
bfleischer <bfleischer@65f8d150-f94c-11dd-8259-6defae454439>
2009-02-12 21:36:00 +0000

    starting off the postie project with what I got

---
+++ trunk/header.inc.php
@@ -0,0 +1,19 @@
+<html><head><title>Postie</title><style type="text/css">
+.hidden {display:none;} 
+.submit {background-color:yellow;}
+textarea {
+	border: 3px solid #cccccc;
+	padding: 5px;
+	font-family: Tahoma, sans-serif;
+	margin:10px auto 0 auto;
+	padding:0;
+}
+textarea.small {
+	width: 600px;
+	height: 25px;
+	}
+textarea.big {
+	width: 600px;
+	height: 400px;
+	}
+</style><script type="text/javascript" src="ajax.js"></script></head><body>
